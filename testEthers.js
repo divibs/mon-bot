f7bb5fab3701dc3712ab7ea866cfdc8889858781
divibs <51919275+divibs@users.noreply.github.com>
2025-02-26 20:07:53 +0800

    Delete testEthers.js

--- testEthers.js
+++
@@ -1,8 +0,0 @@
-const { ethers } = require("ethers"); // CommonJS import
-
-async function main() {
-  const provider = new ethers.JsonRpcProvider("YOUR_RPC_URL");
-  console.log("Provider created:", provider);
-}
-
-main();
